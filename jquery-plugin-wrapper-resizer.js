00866110e951a56aa61a1503bda6986c0c25f671
Naveen Kumar <naveenkumarec405@gmail.com>
2020-03-01 02:26:25 +0530

    initial commit

---
+++ jquery-plugin-wrapper-resizer.js
@@ -0,0 +1,21 @@
+(function ( $ ) {
+
+//to resize parent/wrapper/container width so that all childs are covered
+$.fn.wrapChilds = function(){
+widthResizer($(this));//adjust parent width based on child widths
+return this;
+};
+
+function widthResizer($this){
+console.log("width resizer")
+let prev = 0;
+let updated = 1;
+while(prev!=updated){
+prev=$this[0].scrollWidth;
+$this.width($this[0].scrollWidth+"px");
+updated=$this[0].scrollWidth;
+}
+}
+
+
+})($);
